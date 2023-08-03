e9a657e98eec3f612656b313ac1117a795673e14
KizatovArman <kizatov.armashka.05@gmail.com>
2023-08-03 14:55:46 +0600

    removed unnessecary button background and tint colors added guard let checking for color

--- RandomColorsApp/ColorsDetailsVC.swift
+++ RandomColorsApp/ColorsDetailsVC.swift
@@ -24,16 +24,21 @@ class ColorsDetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Setting background color as passed color
         view.backgroundColor = color ?? UIColor.black
-        let rgb = getRgb(for: color ?? UIColor.black)
+        // Calling getRgb function for passed color to set sliders values as color RGB
+        guard let gColor = color else {
+            return
+        }
+        let rgb = getRgb(for: gColor)
         redColorValueSlider.value = Float(rgb.red)
         greenColorValueSlider.value = Float(rgb.green)
         blueColorValueSlider.value = Float(rgb.blue)
-        getRgbButton.tintColor = UIColor.black
-        getRgbButton.backgroundColor = UIColor.clear
+        // Setting button as selected so its tint color is not blue
         getRgbButton.isSelected = true
     }
     
+    // Function to get Rgb and alpha values from passed color
     func getRgb(for color: UIColor) -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
         var red: CGFloat = 0
         var green: CGFloat = 0
@@ -45,8 +50,13 @@ class ColorsDetailsVC: UIViewController {
         return (red: red, green: green, blue: blue, alpha: alpha)
     }
     
+    // Function that sets new color for view.backgroundColor for RGB components by passed slider value
     func changeCurrentColorFor(component rgbOptions: RGB, by value: CGFloat) {
-        let rgb = getRgb(for: view.backgroundColor ?? UIColor.systemBackground)
+        // Since only one of the components are changing others can be retrieved by getRgb function
+        guard let backgroundColor = view.backgroundColor else {
+            return
+        }
+        let rgb = getRgb(for: backgroundColor)
         switch rgbOptions {
         case .R:
             view.backgroundColor = UIColor(red: value,
@@ -66,33 +76,39 @@ class ColorsDetailsVC: UIViewController {
         }
     }
     
+    // Function that changes view.backgroundColor based on red slider value
     @IBAction func redColorValueSliderChanged(_ sender: UISlider) {
         changeCurrentColorFor(component: .R, by: CGFloat(sender.value))
     }
 
+    // Function that changes view.backgroundColor based on green slider value
     @IBAction func greenColorValueChanged(_ sender: UISlider) {
         changeCurrentColorFor(component: .G, by: CGFloat(sender.value))
     }
     
+    // Function that changes view.backgroundColor based on blue slider value
     @IBAction func blueColorValueChanged(_ sender: UISlider) {
         changeCurrentColorFor(component: .B, by: CGFloat(sender.value))
     }
     
+    // Function that is called when Get RGB button is pressed
     @IBAction func getRgbButtonPressed(_ sender: UIButton) {
-        let viewColor = view.backgroundColor ?? UIColor.black
-        let rgb = getRgb(for: viewColor)
+        // Get RGB values for view.backgroundColor and show that information in alert
+        guard let backgroundColor = view.backgroundColor else {
+            return
+        }
+        let rgb = getRgb(for: backgroundColor)
         let alert = UIAlertController(title: "RGB Values",
                                       message: "Red: \(Int(rgb.red * 255)), Green: \(Int(rgb.green * 255)), Blue: \(Int(rgb.blue * 255))",
                                       preferredStyle: .alert)
         
-                alert.addAction(
-                    UIAlertAction(
-                        title: NSLocalizedString("OK", comment: "This closes alert"),
-                        style: .default, handler:
-                            {_ in NSLog("The \"OK\" alert occured.")}
-                    )
-                )
+        alert.addAction(
+            UIAlertAction(
+                title: NSLocalizedString("OK", comment: "This closes alert"),
+                style: .default
+            )
+        )
 
-                self.present(alert, animated: true, completion: nil)
+        self.present(alert, animated: true, completion: nil)
     }
 }
